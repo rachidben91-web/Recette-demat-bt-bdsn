e8a303073f1af20cc84236825d3e347702b4a4c1
rachidben91-web <rachid.ben91@gmail.com>
2026-02-24 10:38:59 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,15 @@
+// js/supabase.js
+// DEMAT-BT — Connexion Supabase
 
+// ⚠️ Remplace par TES valeurs
+const SUPABASE_URL = "https://tqeemwcnvafqvjnnrdpb.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Z5fcSQtKwqktx_dbsO9nPQ_03HMnden";
+
+// Création du client
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+// Petit log de confirmation
+console.log("✅ Supabase client initialisé");
